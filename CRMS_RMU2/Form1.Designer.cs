7b61d5cde88a1cd5e843b0de0c7de100485ba7a5
石井 孝佳 <ishii@robotsfx.com>
2023-03-10 11:19:32 +0900

    soketで書いてみた。clientとか使ったほうがよいかも。

--- CRMS_RMU2/Form1.Designer.cs
+++ CRMS_RMU2/Form1.Designer.cs
@@ -30,22 +30,171 @@
         {
             this.components = new System.ComponentModel.Container();
             this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.button_comOpen = new System.Windows.Forms.Button();
+            this.textBox_Com = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.mTID設定ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button_clear = new System.Windows.Forms.Button();
+            this.textBox_IP = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_PORT = new System.Windows.Forms.TextBox();
+            this.button_Connect = new System.Windows.Forms.Button();
+            this.connectLamp = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // button_comOpen
+            // 
+            this.button_comOpen.Location = new System.Drawing.Point(92, 35);
+            this.button_comOpen.Name = "button_comOpen";
+            this.button_comOpen.Size = new System.Drawing.Size(75, 23);
+            this.button_comOpen.TabIndex = 1;
+            this.button_comOpen.Text = "Open";
+            this.button_comOpen.UseVisualStyleBackColor = true;
+            this.button_comOpen.Click += new System.EventHandler(this.button_comOpen_Click);
+            // 
+            // textBox_Com
+            // 
+            this.textBox_Com.Location = new System.Drawing.Point(12, 35);
+            this.textBox_Com.Name = "textBox_Com";
+            this.textBox_Com.Size = new System.Drawing.Size(60, 19);
+            this.textBox_Com.TabIndex = 2;
+            this.textBox_Com.Text = "COM3";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mTID設定ToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(452, 28);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // mTID設定ToolStripMenuItem
+            // 
+            this.mTID設定ToolStripMenuItem.Name = "mTID設定ToolStripMenuItem";
+            this.mTID設定ToolStripMenuItem.Size = new System.Drawing.Size(89, 24);
+            this.mTID設定ToolStripMenuItem.Text = "MTID設定";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(55, 24);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // button_clear
+            // 
+            this.button_clear.Location = new System.Drawing.Point(333, 328);
+            this.button_clear.Name = "button_clear";
+            this.button_clear.Size = new System.Drawing.Size(75, 23);
+            this.button_clear.TabIndex = 4;
+            this.button_clear.Text = "Crear";
+            this.button_clear.UseVisualStyleBackColor = true;
+            this.button_clear.Click += new System.EventHandler(this.button_clear_Click);
+            // 
+            // textBox_IP
+            // 
+            this.textBox_IP.Location = new System.Drawing.Point(36, 96);
+            this.textBox_IP.Name = "textBox_IP";
+            this.textBox_IP.ReadOnly = true;
+            this.textBox_IP.Size = new System.Drawing.Size(100, 19);
+            this.textBox_IP.TabIndex = 5;
+            this.textBox_IP.Text = "54.95.4.11";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "IP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "PORT";
+            // 
+            // textBox_PORT
+            // 
+            this.textBox_PORT.Location = new System.Drawing.Point(58, 125);
+            this.textBox_PORT.Name = "textBox_PORT";
+            this.textBox_PORT.ReadOnly = true;
+            this.textBox_PORT.Size = new System.Drawing.Size(100, 19);
+            this.textBox_PORT.TabIndex = 8;
+            this.textBox_PORT.Text = "21008";
+            // 
+            // button_Connect
+            // 
+            this.button_Connect.Location = new System.Drawing.Point(74, 150);
+            this.button_Connect.Name = "button_Connect";
+            this.button_Connect.Size = new System.Drawing.Size(75, 23);
+            this.button_Connect.TabIndex = 9;
+            this.button_Connect.Text = "Connect";
+            this.button_Connect.UseVisualStyleBackColor = true;
+            this.button_Connect.Click += new System.EventHandler(this.button_Connect_Click);
+            // 
+            // connectLamp
+            // 
+            this.connectLamp.Location = new System.Drawing.Point(52, 152);
+            this.connectLamp.Name = "connectLamp";
+            this.connectLamp.ReadOnly = true;
+            this.connectLamp.Size = new System.Drawing.Size(16, 19);
+            this.connectLamp.TabIndex = 10;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(452, 334);
+            this.ClientSize = new System.Drawing.Size(452, 364);
+            this.Controls.Add(this.connectLamp);
+            this.Controls.Add(this.button_Connect);
+            this.Controls.Add(this.textBox_PORT);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_IP);
+            this.Controls.Add(this.button_clear);
+            this.Controls.Add(this.textBox_Com);
+            this.Controls.Add(this.button_comOpen);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Capstone Remote Monitoring System to RMU2 Connect";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button button_comOpen;
+        private System.Windows.Forms.TextBox textBox_Com;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem mTID設定ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.Button button_clear;
+        private System.Windows.Forms.TextBox textBox_IP;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_PORT;
+        private System.Windows.Forms.Button button_Connect;
+        private System.Windows.Forms.TextBox connectLamp;
     }
 }
 
--- CRMS_RMU2/Form1.cs
+++ CRMS_RMU2/Form1.cs
@@ -4,7 +4,10 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,11 +15,189 @@ namespace CRMS_RMU2
 {
     public partial class Form1 : Form
     {
+        int eCode;  //error code
+        MyRichTextBox turbinComTextBox = new MyRichTextBox();
+        string turbinComs="";
+        string turbinCom;
+        //string comBuf;
+        int packetCounter;
+        int MQ;
+        Socket mySocket;
+        bool isComOpen;
+        bool isConnected;
+
         public Form1()
         {
             InitializeComponent();
 
-            serialPort1.BaudRate = 57600;
+            //生データ表示フィールド
+            turbinComTextBox.Location = new Point(307, 48);
+            turbinComTextBox.Size = new Size(118, 274);
+            Controls.Add(turbinComTextBox);
+
+            serialPort1.BaudRate = 19200;
+
+            //turbinComs = "";
+            //turbinCom = "";
+            //comBuf = "";
+            MQ = 0;
+            packetCounter = 0;
+
+            isComOpen = false;
+            isConnected = false;
+        }
+
+        ~Form1()
+        {
+            mySocket.Close();
+        }
+
+        //data set delegate
+        delegate void TextSet(string text);
+
+
+        private void testDisplay()
+        {
+            byte[] data = new byte[1000];
+            //byte[] myCom = new byte[100];
+            byte[] myCom = new byte[100];
+
+            //int dataPos = 0;
+            int dataLen = serialPort1.BytesToRead;
+            int packetLen = 0;
+            int packetPos = 0;
+            //int MQ = 0;
+            //turbinComs += serialPort1.ReadLine();
+            serialPort1.Read(data, 0, dataLen);
+            for (int i = 0; i < dataLen; i++)
+            {
+                switch (MQ)
+                {
+                    case 0:
+                        if (data[i] == 0x01)
+                        {
+                            MQ = 1;
+                            packetLen = 0;
+                        }
+                        break;
+                    case 1:
+                        packetLen = data[i];
+                        MQ = 2;
+                        break;
+                    case 2:
+                        myCom[packetPos] = data[i];
+                        packetPos++;
+                        if (packetPos >= packetLen)
+                        {
+                            //Getstringを使って生成したstringが+=できない現象←StringBuilderを使った
+                            //turbinComs += packetCounter.ToString() + System.Text.Encoding.UTF8.GetString(myCom);
+                            StringBuilder sb = new StringBuilder();
+                            sb.Append(packetCounter.ToString());
+                            for (int j = 0; j < packetLen; j++)
+                            {
+                                sb.Append(Convert.ToChar(myCom[j]));
+                            }
+                            sb.Append("\r\n");
+                            //turbinCom = str + "\r\n";
+                            packetCounter++;
+                            turbinComs += sb;
+                            //turbinComs = String.Concat(turbinComs, str);
+
+                            MQ = 0;
+                        }
+                        break;
+
+                }
+            }
+            Invoke(new TextSet(turbinComTextBox.SetText), new object[] { turbinComs });
+            //Invoke(new TextSet(turbinComTextBox.SetText), new object[] { turbinCom });
+        }
+
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                //testDisplay();
+                if (isConnected)
+                {
+                    byte[] data = new byte[1000];
+                    int dataLen = serialPort1.BytesToRead;
+                    serialPort1.Read(data, 0, dataLen);
+                    mySocket.Send(data, dataLen, SocketFlags.None);
+                }
+                else
+                {
+                    serialPort1.DiscardInBuffer();
+                }
+
+            }
+            catch (System.InvalidOperationException e1)
+            {
+                eCode = 1;
+            }
+            catch (System.ArgumentException e2)
+            {
+                eCode = 2;
+            }
+        }
+
+        private void button_comOpen_Click(object sender, EventArgs e)
+        {
+            serialPort1.PortName = textBox_Com.Text;
+            serialPort1.Open();
+            isComOpen = true;
+        }
+
+        private void button_clear_Click(object sender, EventArgs e)
+        {
+            turbinComs = "";
+            turbinComTextBox.Text = "";
+        }
+
+        private void button_Connect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                IPAddress address = Dns.GetHostAddresses(textBox_IP.Text)[0];
+                IPEndPoint serverEP = new IPEndPoint(address, Int32.Parse(textBox_PORT.Text));
+                mySocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                mySocket.Connect(serverEP);
+                if (mySocket.Connected)
+                {
+                    connectLamp.BackColor = Color.GreenYellow;
+                    isConnected = true;
+
+                    // 受信処理インスタンス生成
+
+                    ReadWorkProc readproc = new ReadWorkProc(Socket, this);
+
+                    // 受信スレッド生成
+                    Thread work = new Thread(new ThreadStart(readproc.Run));
+                    // 受信はバックグラウンドで処理
+                    work.IsBackground = true;
+
+                    work.Start();
+
+
+                }
+            }
+            catch(Exception ex)
+            {
+
+            }
         }
     }
+
+
+    public class MyRichTextBox : RichTextBox
+    {
+        public MyRichTextBox() { }
+
+        public void SetText(string text)
+        {
+            this.Text = text;
+        }
+        
+    }
+
 }
